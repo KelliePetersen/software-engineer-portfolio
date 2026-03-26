545e623ee687e570946db18e6dc90e4ebb64d3ce
KelliePetersen <kelliepetersen@outlook.com>
2026-03-26 21:14:08 +1000

    Move email button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,17 +3,14 @@ import EmailIcon from './icons/EmailIcon'
 function Hero() {
   return (
     <div className="hero">
-      <div className="hero-badge">SOFTWARE ENGINEER</div>
-
-      <p className="hero-tagline">
-        8+ Years Crafting Engaging Websites and Applications
-      </p>
-
-      <h1 className="hero-title">
-        Kellie <br /> Petersen
-      </h1>
-
-      <div className="hero-bottom-wrapper">
+      <div className="hero-wrapper">
+        <div className="hero-badge">SOFTWARE ENGINEER</div>
+        <p className="hero-tagline">
+          8+ Years Crafting Engaging Websites and Applications
+        </p>
+        <h1 className="hero-title">
+          Kellie <br /> Petersen
+        </h1>
         <div className="hero-subheading-wrapper">
           <p className="hero-desc">
             Specialising in React, TypeScript and Clojure.
@@ -23,11 +20,11 @@ function Hero() {
           </p>
           <div className="hero-employment">Currently working at Cubiko</div>
         </div>
-        <a className="hero-email-btn" href="mailto:hello@kelliepetersen.com">
-          <EmailIcon />
-          <span>Email Me</span>
-        </a>
       </div>
+      <a className="hero-email-btn" href="mailto:hello@kelliepetersen.com">
+        <EmailIcon />
+        <span>Email Me</span>
+      </a>
     </div>
   )
 }
